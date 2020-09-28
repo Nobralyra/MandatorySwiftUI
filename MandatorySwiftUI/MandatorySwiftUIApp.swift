eca73a8516746ae94ec3951e16ae0c5ca8aa8f21
Nobralyra <signees96@gmail.com>
2020-09-28 12:12:39 +0200

    Initial Commit

---
+++ MandatorySwiftUI/MandatorySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MandatorySwiftUIApp.swift
+//  MandatorySwiftUI
+//
+//  Created by admin on 28/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct MandatorySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
